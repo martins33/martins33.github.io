2cff2dd15e295c980846bdca33007bb168a253b7
Júlio Martins Reis <juliomreis@tecnico.ulisboa.pt>
2024-05-16 17:41:45 +0100

    +rings

--- js/carrousel.js
+++ js/carrousel.js
@@ -7,10 +7,18 @@ import * as Stats from 'three/addons/libs/stats.module.js';
 /* GLOBAL VARIABLES */
 //////////////////////
 
-var camera, scene, renderer;
+var camera, scene, renderer, controls;
 var ambientLight, directionalLight;
 var geometry, material, mesh;
 var carroussel, cylinder, ring1, ring2, ring3;
+var innerRad = 1,ringThicc = 2,outerRad = 3;
+var extrudeSettings = {
+    amount : 2,
+    steps : 1,
+    depth: 1, //ring heigth
+    bevelEnabled: false,
+    curveSegments: 32
+};
 
 
 /////////////////////
@@ -42,6 +50,8 @@ function createCamera() {
     camera.position.z = 50;
     camera.lookAt(scene.position);
 
+    controls = new OrbitControls( camera, renderer.domElement );
+    controls.update();
     //TODO: create other cameras
 }
 
@@ -113,29 +123,68 @@ function createCylinder(parent, radius, tubularSegments, radialSegments) {
 
 function createRings(parent, cylinderRadius) {
     'use strict';
-
+    
+    //fix to false after testing
     ring1 = new THREE.Object3D();
-    ring1.userData = { goingUp: false, goingDown: false }
+    ring1.userData = { goingUp: true, goingDown: false }
     ring2 = new THREE.Object3D();
-    ring2.userData = { goingUp: false, goingDown: false }
+    ring2.userData = { goingUp: false, goingDown: true }
     ring3 = new THREE.Object3D();
-    ring3.userData = { goingUp: false, goingDown: false }
-
+    ring3.userData = { goingUp: true, goingDown: false }
+/*
     geometry = new THREE.RingGeometry(cylinderRadius, cylinderRadius + 1);
-    material = new THREE.MeshNormalMaterial();  // Normal material
+    material = new THREE.MeshBasicMaterial({color:  Math.random() * 0xffffff,side: THREE.DoubleSide});  // Normal material
     ring1 = new THREE.Mesh(geometry, material);
-    // ring1.rotation.x = Math.PI / 2; // this makes it disappear, for some reason
+    ring1.rotateX(Math.PI / 2); // this makes it disappear, for some reason
     parent.add(ring1);
 
     geometry = new THREE.RingGeometry(cylinderRadius + 1, cylinderRadius + 2);
+    material = new THREE.MeshBasicMaterial({color:  Math.random() * 0xffffff,side: THREE.DoubleSide}); 
     ring2 = new THREE.Mesh(geometry, material);
-    // ring2.rotation.x = Math.PI / 2; // this makes it disappear, for some reason
+    ring2.rotateX(Math.PI / 2); // this makes it disappear, for some reason
     parent.add(ring2);
 
     geometry = new THREE.RingGeometry(cylinderRadius + 2, cylinderRadius + 3);
+    material = new THREE.MeshBasicMaterial({color:  Math.random() * 0xffffff,side: THREE.DoubleSide}); 
     ring3 = new THREE.Mesh(geometry, material);
-    // ring3.rotation.x = Math.PI / 2; // this makes it disappear, for some reason
+    ring3.rotateX(Math.PI / 2); // this makes it disappear, for some reason
     parent.add(ring3);
+*/
+    
+    for(let i=0;i<3;i++){
+        let ring;
+        switch (i){
+            case 0: ring = ring1;
+            break;
+            case 1: ring = ring2;
+            break;
+            case 2: ring = ring3;
+            break;
+        }
+
+        addRing(innerRad,outerRad,ring);
+        innerRad = outerRad;
+        outerRad += ringThicc;
+        
+        ring.position.set(0,1,0);
+        parent.add(ring);
+    }
+}
+
+function addRing(inR,otR,parent){
+    var arcShape = new THREE.Shape();
+    arcShape.absarc(0, 0, otR, 0, Math.PI * 2, 0, false);
+    
+    var holePath = new THREE.Path();
+    holePath.absarc(0, 0, inR, 0, Math.PI * 2,0, true);
+    arcShape.holes.push(holePath);
+    
+    var ring = new THREE.ExtrudeGeometry(arcShape, extrudeSettings);
+    const material = new THREE.MeshBasicMaterial( { color:  Math.random() * 0xffffff, side: THREE.DoubleSide ,wireframe: false} );
+    ring.rotateX(Math.PI/2);
+    const mesh = new THREE.Mesh( ring, material ) ;
+
+    parent.add( mesh )
 }
 
 //TODO: create objects
@@ -198,9 +247,46 @@ function init() {
 /////////////////////
 /* ANIMATION CYCLE */
 /////////////////////
+
+function testing(){
+    
+    if(ring1.userData.goingUp && ring1.position.y <=2){
+        ring1.position.y += 0.1;
+        if(ring1.position.y >=2) {ring1.userData.goingUp = false; ring1.userData.goingDown = true;}
+    }
+    else if(ring1.userData.goingDown && ring1.position.y >=-2){
+        ring1.position.y -= 0.1;
+        if(ring1.position.y <=-2){ ring1.userData.goingUp = true; ring1.userData.goingDown = false;}
+    }
+
+    if(ring2.userData.goingUp && ring2.position.y <=2){
+        ring2.position.y += 0.1;
+        if(ring2.position.y >=2) {ring2.userData.goingUp = false; ring2.userData.goingDown = true;}
+    }
+    else if(ring2.userData.goingDown && ring2.position.y >=-2){
+        ring2.position.y -= 0.1;
+        if(ring2.position.y <=-2){ ring2.userData.goingUp = true; ring2.userData.goingDown = false;}
+    }
+
+    if(ring3.userData.goingUp && ring3.position.y <=2){
+        ring3.position.y += 0.2;
+        if(ring3.position.y >=2) {ring3.userData.goingUp = false; ring3.userData.goingDown = true;}
+    }
+    else if(ring3.userData.goingDown && ring3.position.y >=-2){
+        ring3.position.y -= 0.2;
+        if(ring3.position.y <=-2){ ring3.userData.goingUp = true; ring3.userData.goingDown = false;}
+    }
+
+}
+
 function animate() {
     'use strict';
     //TODO
+    testing();
+    
+    requestAnimationFrame( animate );
+    render();
+    update();
 }
 
 ////////////////////////////
