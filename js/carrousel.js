ab214ecb1f2b8a089704b18e7e226fbcfa163903
Tiago Deane <tiagodeane@tecnico.ulisboa.pt>
2024-05-16 14:44:02 +0100

    initial commit

---
+++ js/carrousel.js
@@ -0,0 +1,151 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
+import * as Stats from 'three/addons/libs/stats.module.js';
+
+//////////////////////
+/* GLOBAL VARIABLES */
+//////////////////////
+
+var camera, scene, renderer, ambientLight;
+
+
+/////////////////////
+/* CREATE SCENE(S) */
+/////////////////////
+function createScene(){
+    'use strict';
+
+    scene = new THREE.Scene();
+
+    scene.background = new THREE.Color(0x000000);
+
+    // Create objects
+
+}
+
+//////////////////////
+/* CREATE CAMERA(S) */
+//////////////////////
+
+function createCamera() {
+    'use strict';
+    camera = new THREE.PerspectiveCamera(20,
+                                         window.innerWidth / window.innerHeight,
+                                         1,
+                                         1000);
+    camera.position.x = 50;
+    camera.position.y = 50;
+    camera.position.z = 50;
+    camera.lookAt(scene.position);
+
+    //TODO: create other cameras
+}
+
+/////////////////////
+/* CREATE LIGHT(S) */
+/////////////////////
+
+function createLights() {
+    'use strict';
+    ambientLight = new THREE.AmbientLight(0xffa500, 0.5);
+    scene.add(ambientLight);
+
+    //TODO: create other lights
+
+}
+
+////////////////////////
+/* CREATE OBJECT3D(S) */
+////////////////////////
+
+//TODO: create objects
+
+//////////////////////
+/* CHECK COLLISIONS */
+//////////////////////
+function checkCollisions(){
+    'use strict';
+    //TODO
+}
+
+///////////////////////
+/* HANDLE COLLISIONS */
+///////////////////////
+function handleCollisions(){
+    'use strict';
+    //TODO
+}
+
+////////////
+/* UPDATE */
+////////////
+function update(){
+    'use strict';
+    //TODO
+}
+
+/////////////
+/* DISPLAY */
+/////////////
+function render() {
+    'use strict';
+    renderer.render(scene, camera);
+}
+
+////////////////////////////////
+/* INITIALIZE ANIMATION CYCLE */
+////////////////////////////////
+function init() {
+    'use strict';
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    createScene();
+    createCamera();
+    createLights();
+
+    render(scene,camera);
+
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("resize", onResize);
+}
+
+/////////////////////
+/* ANIMATION CYCLE */
+/////////////////////
+function animate() {
+    'use strict';
+    //TODO
+}
+
+////////////////////////////
+/* RESIZE WINDOW CALLBACK */
+////////////////////////////
+function onResize() { 
+    'use strict';
+    //TODO
+}
+
+///////////////////////
+/* KEY DOWN CALLBACK */
+///////////////////////
+function onKeyDown(e) {
+    'use strict';
+    //TODO
+}
+
+///////////////////////
+/* KEY UP CALLBACK */
+///////////////////////
+function onKeyUp(e){
+    'use strict';
+    //TODO
+}
+
+init();
+animate();
